b5a5479eb20ce441f432ed487e9d67628baeed02
Daniil Turaev <adef96@mail.ru>
2024-04-02 14:54:22 +0300

    Add garbage-day

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -213,6 +213,16 @@ return {
   },
 
   {
+    "zeioth/garbage-day.nvim",
+    dependencies = "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    opts = {
+      aggressive_mode = true,
+      retries = 4,
+    },
+  },
+
+  {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
